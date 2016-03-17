f5a63adcec7faa5172c7cc6d8e09464bde64a892
Jenner <hypxm@qq.com>
2016-03-17 21:58:33 +0800

    add spider

--- src/spider/Spider.php
+++ src/spider/Spider.php
@@ -43,10 +43,9 @@ class Spider extends Process
                 }
                 $response = $this->http->get($url);
                 $this->cache->set($url, $response->getBody());
+                Logger::info('get body. size:' . strlen($response->getBody()));
             }catch(\Exception $e) {
             }
-
-            Logger::info('get body. size:' . strlen($response->getBody()));
         }
         Logger::info("spider stop");
     }
