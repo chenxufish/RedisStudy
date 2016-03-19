04fb9c1ba54e2fdea098e2e663800ab6f3212283
Jenner <hypxm@qq.com>
2016-03-19 09:56:30 +0800

    test

--- example/unique/hyperloglog.php
+++ example/unique/hyperloglog.php
@@ -10,6 +10,9 @@ require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_S
 
 $file = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'resource' . DIRECTORY_SEPARATOR . 'ip.log';
 $ips = file($file);
+array_walk($ips, function($key, &$value) {
+    $value = trim($value);
+});
 $hyper = new \jenner\redis\study\unique\HyperLogLog($ips);
 $hyper->start();
 
