9bb0556d6b8547de5989bf57a197a2f0526481a9
Jenner <hypxm@qq.com>
2016-03-19 09:49:04 +0800

    add generator

--- example/unique/hyperloglog.php
+++ example/unique/hyperloglog.php
@@ -8,9 +8,8 @@
 
 require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-$file = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'resouce' . DIRECTORY_SEPARATOR . 'ip.log' . PHP_EOL;
+$file = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'resource' . DIRECTORY_SEPARATOR . 'ip.log';
 $ips = file($file);
-var_dump($ips);
 $hyper = new \jenner\redis\study\unique\HyperLogLog($ips);
 $hyper->start();
 
